98f79c4238f53de5e71a382e4557c357926e8b9a
juseongan <juseongan@gmail.com>
2022-05-14 04:46:38 +0900

    fix: fix the potential bug that may cause an error if head.next does not exist

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -11,7 +11,7 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
         # find the middle node
-        slow, fast = head, head.next
+        slow, fast = head, head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
