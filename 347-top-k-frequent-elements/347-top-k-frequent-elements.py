eb9385a008538848ac5c12ed59fe59bfbd675a6d
juseongan <juseongan@gmail.com>
2022-05-23 23:33:28 +0900

    feat: add new python solution for 347-top-k-frequent-elements
    This adds a new python solution using bucket sort. Time complexity is improved from O(nlogK) to O(n).

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -6,3 +6,22 @@ class Solution:
         count = Counter(nums)
         
         return heapq.nlargest(k, count.keys(), key=count.get)
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # bucket sort
+        numMap = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        
+        for num in nums:
+            numMap[num] = 1 + numMap.get(num, 0)
+        for num, count in numMap.items():
+            freq[count].append(num)
+        
+        topK = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                topK.append(n)
+                if len(topK) == k:
+                    return topK
