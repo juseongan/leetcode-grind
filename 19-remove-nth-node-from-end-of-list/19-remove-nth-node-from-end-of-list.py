4ae65e09548fd8b75f3f5aa3e59a0c4ac66b57ef
juseongan <juseongan@gmail.com>
2022-07-16 09:10:25 -0700

    feat: add python solution for 19-remove-nth-node-from-end-of-list

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -1,4 +1,6 @@
 class Solution:
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         dummy = ListNode(0, head)
         left = dummy
