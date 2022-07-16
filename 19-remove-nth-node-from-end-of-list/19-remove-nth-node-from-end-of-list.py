c2f72c3fd4d9e8113605970f41a811607bbab199
juseongan <juseongan@gmail.com>
2022-07-16 09:13:54 -0700

    Revert "feat: add python solution for 19-remove-nth-node-from-end-of-list"
    This reverts commit 4ae65e09548fd8b75f3f5aa3e59a0c4ac66b57ef.

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -1,6 +1,4 @@
 class Solution:
-    # Time Complexity: O(n)
-    # Space Complexity: O(1)
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         dummy = ListNode(0, head)
         left = dummy
