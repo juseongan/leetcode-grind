8135eee0b94eaf6624dfe4c91706dbcb3a91d82e
juseongan <juseongan@gmail.com>
2022-07-19 20:20:18 +0900

    docs: add complexity analysis for 572-subtree-of-another-tree

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -1,17 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
-        if not t: return True
-        if not s: return False
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+    # Time Complexity: O(S*T)
+    # Space Complexity: O(logN)
+        if not subRoot:
+            return True
+        if not root:
+            return False
         
-        if self.sameTree(s, t):
+        if self.isSameTree(root, subRoot):
             return True
-        return (self.isSubtree(s.left, t) or
-                self.isSubtree(s.right, t))
-    
-    def sameTree(self, s, t):
-        if not s and not t:
+        else: # recursively check the subtrees
+            return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    # Time Complexity: O(n)
+    # Space Complexity: O(logN)
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q: # p and q are both None
             return True
-        if s and t and s.val == t.val:
-            return (self.sameTree(s.left, t.left) and
-                    self.sameTree(s.right, t.right))
-        return False
+        
+        if not q or not p or p.val != q.val: # one of p and q is None or have different val
+            return False
+        
+        # recursively check the subtrees
+        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
