0ac379c6a094fdb0b1bbec5576a34e7bdf25466e
juseongan <juseongan@gmail.com>
2022-07-20 20:38:39 +0900

    docs: add complexity analysis for 1448-count-good-nodes-in-binary-tree

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -1,15 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
     def goodNodes(self, root: TreeNode) -> int:
-	
-        def dfs(node, max_so_far):
-            nonlocal num_good_nodes
-            if max_so_far <= node.val:
-                num_good_nodes += 1
-            if node.right:
-                dfs(node.right, max(node.val, max_so_far))
-            if node.left:
-                dfs(node.left, max(node.val, max_so_far))
+        num = 0
         
-        num_good_nodes = 0
-        dfs(root, float("-inf"))
-        return num_good_nodes
+        def dfs(node, maxVal):
+            nonlocal num
+            if maxVal <= node.val:
+                num += 1
+            if node.right:
+                dfs(node.right, max(node.val, maxVal))
+            if node.left:
+                dfs(node.left, max(node.val, maxVal))
+        
+        dfs(root, root.val)
+        return num
