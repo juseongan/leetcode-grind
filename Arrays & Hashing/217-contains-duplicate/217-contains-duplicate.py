ca7b72dc64fcf7229430b9b8f79a1b63c9346113
twinklespirit <hyebin1724@gmail.com>
2022-07-27 07:56:45 +0900

    fix: modify python solution for 217-contains-duplicate
    faster time complexity than previously coded using len()

--- Arrays & Hashing/217-contains-duplicate/217-contains-duplicate.py
+++ Arrays & Hashing/217-contains-duplicate/217-contains-duplicate.py
@@ -1,10 +1,12 @@
+'''
+time complexity reference: https://www.ics.uci.edu/~pattis/ICS-33/lectures/complexitypython.txt
+len(s): O(1) <= because of the 'counter' python has
+set(s): O(len()) <= depends on length of iterable, time complexity of worst case is O(n) 
+'''
 class Solution:
-    # Time Complexity: O(n)
-    # Space Complexity: O(n)
+
     def containsDuplicate(self, nums: List[int]) -> bool:
-        numSet = set()
-        for value in nums: # go through entire array 1 by 1
-            if value in numSet: # check if the duplicate exists in the set
-                return True
-            numSet.add(value)
-        return False
+        if len(nums) == len(set(nums)): 
+            return False
+        else:
+            return True
