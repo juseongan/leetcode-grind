91df9cfc720f88fc8af8ade0895611d3777bd7ca
JuSeong An <juseongan@gmail.com>
2022-08-02 23:29:16 +0900

    feat: add another appraoch for 746-min-cost-climbing-stairs

--- Dynamic Programming/746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ Dynamic Programming/746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -5,3 +5,16 @@ class Solution:
             cost[i] += min(cost[i-1],cost[i-2])
             
         return min(cost[-2],cost[-1])
+
+# Fibonacci Number Solution
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        first = 1
+        second = 2
+        for i in range(3, n+1):
+            third = first + second
+            first = second
+            second = third
+        return second
