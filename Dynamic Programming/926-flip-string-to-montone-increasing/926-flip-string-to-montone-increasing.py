45934d667eae65bc0b6e9b9f6c7955824b6782bd
Hyochan Chong <d7chong@gmail.com>
2022-08-08 04:34:55 +0900

    feat: commit 926-flip-string-to-montone-increasing

---
+++ Dynamic Programming/926-flip-string-to-montone-increasing/926-flip-string-to-montone-increasing.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        
+        s = s[s.find("1"):]
+        s = s[:s.rfind("0")+1]
+        if len(s) == 0:
+            return 0
+        
+        # Initialize the dp table
+        dp = [[0],[0]]
+        
+        # Create the dp table
+        L = len(s)
+        row, cnt = 0, 1
+        for idx in range(L-1):
+            if s[idx] != s[idx+1]:
+                dp[row].append(cnt)
+                row = (row + 1) % 2
+                cnt = 1
+            else:
+                cnt += 1
+        dp[row].append(cnt)
+        
+        # Make the dp table into a dp sum table
+        N = len(dp[0])
+        for j in range(1,N):
+            dp[0][j] += dp[0][j-1]
+            dp[1][j] += dp[1][j-1]
+        
+        # Calculate the answer
+        flips = 100001
+        for j in range(N):
+            flips = min(flips, dp[1][N-1] - dp[1][j] + dp[0][j])
+        return flips
