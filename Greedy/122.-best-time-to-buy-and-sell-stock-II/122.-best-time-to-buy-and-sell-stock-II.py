a145e1f963e8b71de212331b1abd164c728f8a3b
twinklespirit <hyebin1724@gmail.com>
2022-08-10 07:28:11 +0900

    docs: add a python solution using pythonic method for 122.-best-time-to-buy-and-sell-stock-II

--- Greedy/122.-best-time-to-buy-and-sell-stock-II/122.-best-time-to-buy-and-sell-stock-II.py
+++ Greedy/122.-best-time-to-buy-and-sell-stock-II/122.-best-time-to-buy-and-sell-stock-II.py
@@ -1,7 +1,8 @@
 '''
-
+Sell before the stock price falls and buy before it rises.
 '''
 
+# Greedy
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # Declare variable
@@ -15,6 +16,17 @@ class Solution:
                 
         return gain
             
+'''
+Time Complexity: O(n)
+Space Complexity: O(1)
+'''
+
+# Pythonic way
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # 매번 이익일 때마다 합산
+        return sum(max(prices[i+1]-prices[i], 0) for i in range(len(prices)-1))
+
 '''
 Time Complexity: O(n)
 Space Complexity: O(1)
