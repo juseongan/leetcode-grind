b45aa55cb87bd1d807d8b7678a25abcb24887fc0
twinklespirit <hyebin1724@gmail.com>
2022-08-13 22:45:53 +0900

    feat: add another approach for 125-valid-palindrome

--- Two Pointers/125-valid-palindrome/125-valid-palindrome.py
+++ Two Pointers/125-valid-palindrome/125-valid-palindrome.py
@@ -17,3 +17,38 @@ class Solution:
             r -= 1
         
         return True
+
+
+
+'''
+Explan how to solve
+1. 먼저는 uppercase를 lowercase로 바꾸고 non-alphnumeric characters를 없앤다.
+    string_name.lower(), string_name.isalnum()
+2. palindrome인지 판별한다.
+    2.1 주어진 s 문자열을 reverse 하여 reversed_s에 저장한다.
+    2.2 s와 reversed_s가 같은지 비교한다.
+3. palindrome이라면 true, 아니라면 false를 반환한다.
+'''
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        # uppercase를 lowercase로 바꾸고 non-alphnumeric characters를 없앤다.
+        s = ''.join(filter(str.isalnum, s))
+        s = s.lower()
+        
+        # palindrome인지 판별한다.
+        reversed_s = "".join(reversed(s))
+        if s == reversed_s:
+            # palindrome이라면 true
+            return "true"
+        # 아니라면 false를 출력한다.
+        else: "false"
+        
+'''
+Time Complexity: T(3n + c) = O(n)
+Space Complexity: O(1)
+
+As you can see in the CPython function, there’s no auxiliary space involved and there’s no use of recursion either. Hence the space complexity of the operation is constant i.e O(1).
+refer to: https://thecodingbot.com/time-and-space-complexity-analysis-of-pythons-list-reverse-method/#:~:text=Time%20Complexity%3A%20O(N),third%20last%2C%20and%20so%20on.
+'''
