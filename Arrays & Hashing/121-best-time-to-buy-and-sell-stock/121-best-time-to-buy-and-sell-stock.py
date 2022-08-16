2896da729865686df7463e52184b0eaf8dbd1cf4
twinklespirit <hyebin1724@gmail.com>
2022-08-17 04:04:08 +0900

    docs: add and modify comments for 121-best-time-to-buy-and-sell-stock

--- Arrays & Hashing/121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ Arrays & Hashing/121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,26 +1,31 @@
 '''
-1. We need to find the minimum price of prices[i]
-2. We need to find the maximum price of prices[i] after prices[minimum] position
-3. We need to return the profit by caculating maximum - mimimum
+1. find the minimum price of prices
+2. find the maximum price of prices after minimum price
+3. return the profit by caculating maximum - mimimum
 '''
 
-
+# Dynamic Programming 
 class Solution:
     # input: prices[i] is the price of a given stock on the i day
     def maxProfit(self, prices: List[int]) -> int:
         
         # Declare varibale
         min_price = sys.maxsize 
-        profit = 0
+        max_profit = 0
         
         # Find the maximum profit
         for price in prices:
             min_price = min(min_price, price)
-            profit = max(profit, price - min_price)
-            
-            
-        return profit
-    
+            '''
+            if prices[price] < min_price:
+                min_price = prices[price] 
+            '''
+            max_profit = max(max_profit, price - min_price)
+            '''
+            elif prices[price] - min_price > max_profit:
+                max_profit = prices[price] - min_price
+            '''
+        return max_profit  
 '''
 Time Complexity: O(n) 
 Space Complexity: O(1) 
