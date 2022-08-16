e274f0778287c4e466af1fcc50fd02d210ce48fd
JuSeong An <juseongan@gmail.com>
2022-08-16 21:44:25 +0900

    feat: add another approach for 670-maximum-swap.py

--- Greedy/670-maximum-swap/670-maximum-swap.py
+++ Greedy/670-maximum-swap/670-maximum-swap.py
@@ -28,3 +28,22 @@ num = int(num)
 
 # Return int
 print(num)
+
+class Solution:
+    # Author: juseongan
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    def maximumSwap(self, num: int) -> int:
+        numList = [int(char) for char in str(num)]
+        
+        max_idx = len(numList) - 1
+        earliest = latest = 0
+        for idx in range(len(numList)-1, -1, -1):
+            if numList[idx] > numList[max_idx]:     # update the max since current value is larger
+                max_idx = idx
+            elif numList[idx] < numList[max_idx]:   # switch the max with the earliest int possible
+                earliest = idx
+                latest = max_idx
+            
+        numList[earliest], numList[latest] = numList[latest], numList[earliest]
+        return int("".join([str(digit) for digit in numList]))
