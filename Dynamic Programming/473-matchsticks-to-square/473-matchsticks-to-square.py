617bd568f4e0f9ccb5bd7b0dba4ca69b63a5a470
twinklespirit <hyebin1724@gmail.com>
2022-08-18 08:14:31 +0900

    feat: another approach for 473-matchsticks-to-square

--- Dynamic Programming/473-matchsticks-to-square/473-matchsticks-to-square.py
+++ Dynamic Programming/473-matchsticks-to-square/473-matchsticks-to-square.py
@@ -33,3 +33,42 @@ Space Complexity:
 
 refer to: https://www.youtube.com/watch?v=hUe0cUKV-YY
 '''
+
+
+
+# dfs
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        
+        # 전체 둘레로 한 변의 길이가 정수인지 파악
+        perimeter = sum(matchsticks) # perimeter is 둘레  
+        if perimeter % 4 != 0: return False
+        else: side = perimeter // 4  # side is 변
+        
+        matchsticks.sort(reverse=True)
+        # pass the lengths at each side
+        def dfs(w,e,n,s,index): # west, east, north, south
+            # base case
+            # if we find 
+            if index==len(matchsticks):
+                if w==e==n==s: return True
+                return False
+            m = matchsticks[index]
+            
+            '''
+            if dfs(w+m,e,n,s,index+1): waste time on 
+            '''
+            if w+m <= side and dfs(w+m,e,n,s,index+1): return True
+            if e+m <= side and dfs(w,e+m,n,s,index+1): return True
+            if n+m <= side and dfs(w,e,n+m,s,index+1): return True
+            if s+m <= side and dfs(w,e,n,s+m,index+1): return True
+            
+            return False
+        
+        return dfs(0,0,0,0,0)
+'''
+Time Complexity: 
+Space Complexity: 
+
+refer to: https://www.youtube.com/watch?v=Amfd1bqZmVg&t=90s
+'''
