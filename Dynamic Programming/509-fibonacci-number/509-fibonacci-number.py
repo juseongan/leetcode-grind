05b5b3ea03740dea601edfd99aaaeb180b125846
JuSeong An <juseongan@gmail.com>
2022-08-19 08:43:08 -0700

    feat: add 2 other python approach for 509-fibonacci-number

--- Dynamic Programming/509-fibonacci-number/509-fibonacci-number.py
+++ Dynamic Programming/509-fibonacci-number/509-fibonacci-number.py
@@ -1,4 +1,16 @@
+# Recursion Approach
 class Solution:
+    # Time Complexity: O(2^n) exponential time
+    # Space Complexity: O(n)
+    def fib(self, N: int) -> int:
+        if N <= 1:
+            return N
+        return self.fib(N - 1) + self.fib(N - 2)
+
+# Bottom-Up Approach with Tabulation
+class Solution:
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
     def fib(self, N: int) -> int:
         if N <= 1:
             return N
@@ -9,3 +21,15 @@ class Solution:
             cache[i] = cache[i - 1] + cache[i - 2]
 
         return cache[N]
+
+# Top-Down Approach with Memoization
+class Solution:
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    cache = {0: 0, 1: 1}
+
+    def fib(self, N: int) -> int:
+        if N in self.cache:
+            return self.cache[N]
+        self.cache[N] = self.fib(N - 1) + self.fib(N - 2)
+        return self.cache[N]
