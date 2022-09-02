f7b1c04c9780a4acbf5654fbcd5c5bf68e57b276
JuSeong An <juseongan@gmail.com>
2022-09-01 21:36:51 -0700

    feat: add python solution for 637-average-of-levels-in-binary-tree

---
+++ Trees/637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
@@ -0,0 +1,13 @@
+def averageOfLevels(self, root):
+    info = []
+    def dfs(node, depth = 0):
+        if node:
+            if len(info) <= depth:
+                info.append([0, 0])
+            info[depth][0] += node.val
+            info[depth][1] += 1
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+    dfs(root)
+
+    return [s/float(c) for s, c in info]
