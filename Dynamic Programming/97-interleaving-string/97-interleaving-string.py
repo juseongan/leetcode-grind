def9b9f0658694d25ea88b5ba721b320c0a32f62
JuSeong An <juseongan@gmail.com>
2022-09-07 23:08:02 -0700

    feat: add another approach for 97-interleaving-string

--- Dynamic Programming/97-interleaving-string/97-interleaving-string.py
+++ Dynamic Programming/97-interleaving-string/97-interleaving-string.py
@@ -56,3 +56,27 @@ class Solution:
                 if s2[idx2]==s3[idx3]:
                     queue.append((idx1, idx2+1, idx3+1))
         return False
+
+
+# Recursion(dfs) with cache
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        
+        dp = {}
+        def dfs(i1, i2):
+            if i1 == len(s1) and i2 == len(s2):
+                return True
+            if (i1, i2) in dp:
+                return dp[(i1,i2)]
+            
+            if i1 < len(s1) and s1[i1] == s3[i1+i2] and dfs(i1+1, i2):
+                return True
+            if i2 < len(s2) and s2[i2] == s3[i1+i2] and dfs(i1, i2+1):
+                return True
+            
+            dp[(i1, i2)] = False
+            return False
+        
+        return dfs(0, 0)
