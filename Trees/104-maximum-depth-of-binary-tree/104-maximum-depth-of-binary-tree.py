7f1472877ffb55b87a94a0e2948a547c29426830
twinklespirit <hyebin1724@gmail.com>
2022-09-15 04:11:07 +0900

    feat: add a python solution for 102-maximum-depth-of-binary-tree

--- Trees/104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ Trees/104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -16,3 +16,35 @@ class Solution:
             rightHeight = self.maxDepth(root.right) 
             
             return max(leftHeight, rightHeight) + 1 
+
+
+
+
+# hyebin's solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        
+        if root is None: return 0
+        
+        queue = collections.deque([root])
+        depth = 0
+        
+        while queue:
+            depth += 1
+            
+            for _ in range(len(queue)):
+                current_node = queue.popleft()
+                
+                if current_node.left: 
+                    queue.append(current_node.left)
+                if current_node.right: 
+                    queue.append(current_node.right)
+            
+        return depth
