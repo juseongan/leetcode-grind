8baa5fb154208868492a64d019f8d3f3c483fa78
twinklespirit <hyebin1724@gmail.com>
2022-09-15 04:16:25 +0900

    feat: add a python solution for 226-invert-binary-tree

--- Trees/226-invert-binary-tree/226-invert-binary-tree.py
+++ Trees/226-invert-binary-tree/226-invert-binary-tree.py
@@ -21,3 +21,32 @@ class Solution:
         self.invertTree(root.right)
         
         return root
+
+
+
+
+# hyebin's solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        queue = collections.deque([root])
+        
+        while queue:
+            current_node = queue.popleft()
+    
+            
+            if current_node:
+                current_node.left, current_node.right = current_node.right, current_node.left
+                
+                queue.append(current_node.left)
+                queue.append(current_node.right)
+                
+        return root
+                
+        
