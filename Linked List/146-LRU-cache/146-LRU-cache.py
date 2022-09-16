cb58b06c3b992f329b7ef878a82e4b47c5372d6f
JuSeong An <juseongan@gmail.com>
2022-09-16 22:53:15 +0900

    feat: add another approach for 146-LRU-cache

--- Linked List/146-LRU-cache/146-LRU-cache.py
+++ Linked List/146-LRU-cache/146-LRU-cache.py
@@ -1,8 +1,8 @@
 # Ordered Dictionary Approach
+# Time Complexity: O(1)
+# Space Complexity: O(n)
 from collections import OrderedDict
 class LRUCache(OrderedDict):
-    # Time Complexity: O(1)
-    # Space Complexity: O(n)
     def __init__(self, capacity: int):
         self.capacity = capacity
 
@@ -21,3 +21,67 @@ class LRUCache(OrderedDict):
         
         if len(self) > self.capacity:
             self.popitem(last = False)
+
+
+# Hashmap + DoubleLinkedList Approach
+# Time Complexity: O(1)
+# Space Complexity: O(n)
+class ListNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.dic = dict() # key to node
+        self.capacity = capacity
+        self.head = ListNode(0, 0)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key in self.dic:
+            node = self.dic[key]
+            self.removeFromList(node)
+            self.insertIntoHead(node)
+            return node.value
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:             # similar to get()        
+            node = self.dic[key]
+            self.removeFromList(node)
+            self.insertIntoHead(node)
+            node.value = value         # replace the value len(dic)
+        else: 
+            if len(self.dic) >= self.capacity:
+                self.removeFromTail()
+            node = ListNode(key,value)
+            self.dic[key] = node
+            self.insertIntoHead(node)
+    
+    def removeFromList(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    
+    def insertIntoHead(self, node):
+        headNext = self.head.next 
+        self.head.next = node 
+        node.prev = self.head 
+        node.next = headNext 
+        headNext.prev = node
+    
+    def removeFromTail(self):
+        if len(self.dic) == 0: return
+        tail_node = self.tail.prev
+        del self.dic[tail_node.key]
+        self.removeFromList(tail_node)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
