4d661be6a43fd6209d2fb12f6a43b0abd1524738
twinklespirit <hyebin1724@gmail.com>
2022-09-18 02:40:02 +0900

    feat: add another approach for 234-palindrome-linked-list

--- Linked List/234-palindrome-linked-list/234-palindrome-linked-list.py
+++ Linked List/234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -1,21 +1,67 @@
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+'''
+hyebin's solution
+Way1. convert linked list to list 
+'''
+# https://leetcode.com/problems/palindrome-linked-list
 
-# 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
+
+'''
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        node = head
         queue = []
-
-        # head is node
-        while head is not None:
+        
+        # convert linked list to list
+        while node is not None: # check empty List
             queue.append(node.val)
             node = node.next
-            
-        while len(q) > 1:
-            if queue.pop(0) != queue.pop():
-                return False
         
+        # check palindrome
+        '''
+        same as 'return queue == queue[::-1]'
+        '''
+        while len(queue) > 1:               
+            if queue.pop(0) != queue.pop(): 
+                return False
         return True
+'''
+time complexity: O(n^2)
+space complexity: O(n) 'queue'
+'''
+
+# way2. deque (faster than above)
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        queue = collections.deque()
+        
+        # check empty list
+        if not head: 
+            return True
+        
+        node = head
+        while node is not None:
+            queue.append(node.val)
+            node = node.next            
+            
+        while len(queue)>1:
+            if queue.popleft() != queue.pop():
+                return False
+        return True
+'''
+time complexity: O(n)
+space complexity: O(n)
+'''
