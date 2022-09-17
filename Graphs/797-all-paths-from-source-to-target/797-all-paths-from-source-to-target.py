e59785fb070cd7872940f9b557449b647d7f2b39
twinklespirit <hyebin1724@gmail.com>
2022-09-17 16:28:10 +0900

    fix: fix conflict for auto merge in 797-all-paths-from-source-to-target

--- Graphs/797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ Graphs/797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,47 +1,27 @@
+# hyochan's solution
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        
+
         N = len(graph)
         paths = []
-        
+
         def dfs(here, path):
             if here == N-1:
                 paths.append(path)
                 print(path)
-            
+
             for there in graph[here]:
                 dfs(there, path + [there])
-        
+
         dfs(0, [0])
-        
+
         return paths
-    
 
+
+
+
+# hyebin's solution
 # iteractive (stack) -> Wrong
-
-class Solution:
-    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        
-        target = len(graph) - 1 
-        path = [] 
-        visited = []
-        
-        def dfs(source, target, visited):
-            visited.append(source)
-            if source == target:
-                path.append(visited.copy())
-                
-            for adjacent_vertex in graph[source]:
-                dfs(adjacent_vertex, target, visited)
-                
-            visited.pop()
-            return path
-        
-        return dfs(0, target, visited)
-    
-    
-# iteractive (stack) -> Wrong
-
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
 
