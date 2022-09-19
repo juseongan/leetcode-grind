10dd75b16afc3b68647c0bc63665a30f348cab67
twinklespirit <hyebin1724@gmail.com>
2022-09-19 11:31:07 +0900

    feat: add a python solution for 110-balanced-binary-tree

--- Trees/110-balanced-binary-tree/110-balanced-binary-tree.py
+++ Trees/110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -24,3 +24,31 @@ class Solution:
         if treeDepth(root) >= MAX:
             return False
         return True
+
+
+
+# hyebin's solution (recursive way)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def check(root):
+            if root is None: return -1
+            
+            left_height = check(root.left)
+            right_height = check(root.right)
+            
+            if abs(left_height - right_height) > 1 or left_height == -2 or right_height == -2:
+                return -2
+            return max(left_height, right_height) + 1
+        
+        return check(root) != -2
+'''
+time complexity: 
+space complexity: 
+'''
