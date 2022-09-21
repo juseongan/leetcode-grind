1a8fa154139ac05048637bfa775ac767c2c36723
JuSeong An <juseongan@gmail.com>
2022-09-21 16:12:10 +0900

    feat: add iterative DFS approach for 133-clone-graph

--- Graphs/133-clone-graph/133-clone-graph.py
+++ Graphs/133-clone-graph/133-clone-graph.py
@@ -21,3 +21,27 @@ class Solution:
                 curClone.neighbors.append(clones[neighbor.val])       
         
         return clones[node.val]
+
+# Iterative DFS Approach
+class Solution:
+    # Time Complexity: O(V + E)
+    # Space Complexity: O(V)
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+        
+        stack = [node]
+        clones = {node.val: Node(node.val)}
+        
+        while stack:
+            curNode = stack.pop()
+            curClone = clones[curNode.val]
+            
+            for neighbor in curNode.neighbors:
+                if neighbor.val not in clones:
+                    clones[neighbor.val] = Node(neighbor.val)
+                    stack.append(neighbor)
+                
+                curClone.neighbors.append(clones[neighbor.val])       
+        
+        return clones[node.val]
