bdef5d766fa80bad3a9ac946d320647c870eedf7
twinklespirit <hyebin1724@gmail.com>
2022-09-23 04:56:16 +0900

    feat: add a python solution for 94-binary-inorder-traversal

--- Trees/94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ Trees/94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -18,3 +18,29 @@ class Solution:
             self.helper(node.left, traversal)
             traversal.append(node.val)
             self.helper(node.right, traversal)
+
+# hyebin's solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        inorder_traversal = []
+        
+        def inorder(root, inorder_traversal):
+            if root:
+                inorder(root.left, inorder_traversal)
+                inorder_traversal.append(root.val)
+                inorder(root.right, inorder_traversal)
+            
+        inorder(root, inorder_traversal)
+        return inorder_traversal
+'''
+n is the number of nodes
+Time complexity: 2T(n/2) = O(n)
+Space complexity: O(n)
+'''
