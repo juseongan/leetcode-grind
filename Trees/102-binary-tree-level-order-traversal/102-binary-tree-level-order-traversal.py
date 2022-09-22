f08d8d62f627a2378d71b455227f2aaa126322e3
twinklespirit <hyebin1724@gmail.com>
2022-09-23 06:05:38 +0900

    feat: add a python solution for 102-binary-tree-level-order-traversal

--- Trees/102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ Trees/102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -31,3 +31,29 @@ class Solution:
             level += 1 # next level
         
         return levels
+
+# hyebin's solution
+# way1. bfs
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        level_order = []
+        queue = collections.deque([root] if root else [])
+        
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left: 
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            level_order.append(level)
+        return level_order
