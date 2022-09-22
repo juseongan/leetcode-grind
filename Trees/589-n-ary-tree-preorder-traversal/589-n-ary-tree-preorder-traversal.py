c4ba7142c24e4756c14fcd31fe5aec0c98bcd2b3
twinklespirit <hyebin1724@gmail.com>
2022-09-23 05:42:18 +0900

    feat: add another literative approach for 589-n-ary-tree-preorder-traversal

--- Trees/589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
+++ Trees/589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
@@ -19,3 +19,25 @@ class Solution:
             n_ary_preorder.append(root.val)
             for child in root.children:
                 self.nary(child, n_ary_preorder)
+
+# hyebin's solution
+# way2. literative
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if root is None: return []
+        stack = [root]
+        n_ary_tree = []
+        
+        while stack:
+            temp = stack.pop()
+            n_ary_tree.append(temp.val)
+            stack.extend(temp.children[::-1])
+        return n_ary_tree
