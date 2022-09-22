fe4c8b41fe7c57217b79656d5b5894f4c2289f83
twinklespirit <hyebin1724@gmail.com>
2022-09-23 05:53:38 +0900

    feat: add a python solution for 429-n-ary-tree-level-order-traversal

--- Trees/429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.py
+++ Trees/429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.py
@@ -22,3 +22,26 @@ class Solution:
                 level += [node.val]
             order.append(level)
         return order
+
+# hyebin's solution
+# way1. bfs
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        level_order = []
+        queue = collections.deque([root] if root else [])
+
+        while queue:
+            level_order.append([])
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level_order[-1].append(node.val)
+                queue.extend(node.children)
+        return level_order
