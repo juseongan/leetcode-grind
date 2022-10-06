284197a3ca317cba90f77b8f18838afcfe7992c6
twinklespirit <hyebin1724@gmail.com>
2022-10-06 23:47:20 +0900

    docs: add a jusoeng's solution and write comments in hyebin's solution

--- Backtracking/17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ Backtracking/17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,3 +1,24 @@
+# Juseong's solution
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        letterMap = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        
+        def backtrack(idx, combination):
+            # if the path is the same length as digits, we have a complete combination
+            if len(combination) == len(digits):
+                combinations.append(combination)
+                return
+            
+            # loop through all the possible letters
+            for letter in letterMap[digits[idx]]:
+                backtrack(idx + 1, combination + letter)
+            
+        combinations = []
+        if digits:
+            backtrack(0, "")
+            
+        return combinations
+
 # Hyochan's solution
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
@@ -28,7 +49,7 @@ class Solution:
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         
-        # represent telephone buttons to graph
+        # represents telephone buttons to graph
         buttons = {
             "2":"abc",
             "3":"def",
@@ -41,19 +62,25 @@ class Solution:
         }
         
         if not digits: 
-            return [] # if digits is "", return []
+            return [] # if digits are "", return []
         
         combinations = []
-        def dfs(combination, i):
-            # 종료 조건
+        def dfs(combination, index):
+            # If I combine each digit of 'digits', I will store the result of combining and backtrack
             if len(combination) == len(digits):
                 combinations.append(combination)
                 return
             
-            for i in range(i, len(digits)): # i is index of digits
-                number = digits[i]
-                strings = buttons[number]
+            # Access each element of 'digits'
+            # If i write 'for digit in digits' -> we can not move to next index's string. because 'str' can concatenate only 'str'
+            # so I will use 'index'
+            # Let the first element's index of 'digits' be i
+            # 1 character of the 'i'th string and concatenates each charater in the 'i+1'th string
+            for i in range(index, len(digits)): # you can write 'index' as 'i'.
+                digit = digits[i]
+                strings = buttons[digit]
                 for string in strings:
+                    # If the i-th character is picked, the search begins
                     dfs(combination+string, i+1)    
                 
         dfs("", 0)
